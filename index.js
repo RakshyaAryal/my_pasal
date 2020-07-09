a14ce1d2b0da63168092eb5b5ee9838268a0c36f
Kiran Mulmi <kiran.mulmi@monotype.com>
2020-07-09 08:57:48 +0545

    array object

--- index.js
+++ index.js
@@ -225,5 +225,6 @@ for (let i = 0; i < mobiles.length; i++) {
  * https://www.w3schools.com/js/js_array_methods.asp
  * https://www.w3schools.com/js/js_array_sort.asp
  * https://www.w3schools.com/js/js_array_iteration.asp
+ * https://www.w3schools.com/js/js_loop_for.asp
  * */
 
